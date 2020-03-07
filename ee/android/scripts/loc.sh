81d2f2ebf4532005f708b5458b7934c69afbdbe1
G. Blake Meike <blake.meike@couchbase.com>
2020-03-06 17:43:44 -0800

    Delete obsolete stuff

--- ee/android/scripts/loc.sh
+++
@@ -1,9 +0,0 @@
-echo ""
-echo "Java: "
-cloc . --include-ext=java --quiet
-
-echo ""
-echo "JNI: "
-pushd libs/couchbase-lite-android/libs/couchbase-lite-core/Java > /dev/null
-cloc . --include-ext=h,cc --quiet
-popd
