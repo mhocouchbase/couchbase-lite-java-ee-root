db611a8ad04d93b5fbc6a4298d6681260b771269
G. Blake Meike <blake.meike@couchbase.com>
2020-04-15 16:46:02 -0700

    Get build working again

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,6 +4,7 @@ node {
     build job:
         'couchbase-lite-android-ci',
         parameters: [
+            string(name:'PR_REPO_URL', value:'git@github.com:couchbaselabs/couchbase-lite-java-ee-root.git'),
             string(name:'PR_BRANCH', value:"${env.CHANGE_BRANCH}"),
             string(name:'PR_NAME', value:"${env.CHANGE_ID}-${PR_NAME}")
         ]
--- ee/android/etc/jenkins/build.sh
+++ ee/android/etc/jenkins/build.sh
@@ -10,13 +10,15 @@ NDK_VERSION='20.1.5948944'
 CMAKE_VERSION='3.10.2.4988404'
 BUILD_TOOLS_VERSION='29.0.3'
 
+MAVEN_URL="http://mobile.maven.couchbase.com/maven2/cimaven"
+
 
 function usage() {
-    echo "Usage: $0 <sdk path>"
+    echo "Usage: $0 <sdk path> <build number>"
     exit 1
 }
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ]; then
     usage
 fi
 
@@ -25,9 +27,14 @@ if [ -z "$SDK_HOME" ]; then
     usage
 fi
 
+BUILD_NUMBER="$2"
+if [ -z "$BUILD_NUMBER" ]; then
+    usage
+fi
+
 SDK_MGR="${SDK_HOME}/tools/bin/sdkmanager"
 
-echo "======== CHECK Couchbase Lite Java Family, Enterprise Edition v`cat version.txt`"
+echo "======== BUILD Couchbase Lite Android, Enterprise Edition v`cat ../../version.txt`-${BUILD_NUMBER}"
 
 echo "======== Install Toolchain"
 yes | ${SDK_MGR} --licenses > /dev/null 2>&1
@@ -35,13 +42,17 @@ ${SDK_MGR} --install "build-tools;${BUILD_TOOLS_VERSION}"
 ${SDK_MGR} --install "cmake;${CMAKE_VERSION}"
 ${SDK_MGR} --install "ndk;${NDK_VERSION}"
 
+# The Jenkins script has already put passwords into local.properties
 cat <<EOF >> local.properties
 sdk.dir=${SDK_HOME}
 ndk.dir=${SDK_HOME}/ndk/${NDK_VERSION}
 cmake.dir=${SDK_HOME}/cmake/${CMAKE_VERSION}
 EOF
 
-echo "======== Check"
-./gradlew ciCheck -PtargetAbis=arm64-v8a || exit 1
+echo "======== Build"
+./gradlew ciBuild -PbuildNumber="${BUILD_NUMBER}" || exit 1
 
-echo "======== CHECK COMPLETE"
+echo "======== Publish artifacts"
+./gradlew ciPublish -PbuildNumber="${BUILD_NUMBER}" -PmavenUrl="${MAVEN_URL}" || exit 1
+
+echo "======== BUILD COMPLETE"
