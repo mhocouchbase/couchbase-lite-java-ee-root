0cbdbb1b7394089a414b173e8b12e6ebad78d906
G. Blake Meike <blake.meike@couchbase.com>
2020-04-24 17:01:32 -0700

    Fix Java build and move to Centos7

---
+++ ee/java/etc/jenkins/publish_windows.bat
@@ -0,0 +1,27 @@
+@echo ON
+
+:: Publish Couchbase Lite Java, Enterprise Edition for Windows
+
+SET product='couchbase-lite-java-ee'
+
+if "%3%" == "" (
+    echo Usage: publish_windows.bat ^<VERSION^> ^<BUILD_NUMBER^> ^<ARTIFACTS^>
+    exit /B 1
+)
+
+set version=%1%
+set buildNumber=%2%
+set artifactDir=%3%
+
+
+echo ======== PUBLISH Couchbase Lite Java, Enterprise Edition  
+
+copy "lib/build/distributions/"%product%-%version%-%buildNumber%.zip" "%artifactsDir%/%product%-%version%-%buildNumber%-windows.zip"
+
+echo ======== PUBLICATION COMPLETE
+
+goto :eof
+
+:error
+echo Failed with error %ERRORLEVEL%.
+exit /b %ERRORLEVEL%
