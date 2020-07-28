a90db0460a130c3ce71a6118d9193146d2a20736
G. Blake Meike <blake.meike@couchbase.com>
2020-07-28 12:45:30 -0700

    Update Core

---
+++ tools/verify_core.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+NEXUS_URL="http://nexus.build.couchbase.com:8081/nexus/content/repositories/releases/com/couchbase/litecore"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+TOOLS_DIR="${SCRIPT_DIR}/../common/tools"
+
+"${TOOLS_DIR}/fetch_litecore.sh" -e EE -n "${NEXUS_URL}" 
