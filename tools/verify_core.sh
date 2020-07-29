827a8c940e6b4fddb441c64b4d2f914215c30999
G. Blake Meike <blake.meike@couchbase.com>
2020-07-29 13:57:52 -0700

    Fix Windows build

--- tools/verify_core.sh
+++ tools/verify_core.sh
@@ -4,4 +4,13 @@ NEXUS_URL="http://nexus.build.couchbase.com:8081/nexus/content/repositories/rele
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 TOOLS_DIR="${SCRIPT_DIR}/../common/tools"
 
-"${TOOLS_DIR}/fetch_litecore.sh" -e EE -n "${NEXUS_URL}" 
+mkdir .core-tmp
+pushd .core-tmp
+
+HERE=`pwd`
+
+"${TOOLS_DIR}/fetch_litecore.sh" -e EE -n "${NEXUS_URL}" -o "${HERE}" 
+
+popd
+rm -rf .core-tmp
+
