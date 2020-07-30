8775ed192206a3b49166aeaa4a9cbbf45381b488
G. Blake Meike <blake.meike@couchbase.com>
2020-07-30 11:34:38 -0700

    Fix Centos6 build

--- ee/java/etc/jenkins/test_linux.sh
+++ ee/java/etc/jenkins/test_linux.sh
@@ -3,27 +3,33 @@
 # Test Couchbase Lite Java, Enterprise Edition
 #
 function usage() {
-    echo "Usage: $0 <build number> <reports path>"
+    echo "Usage: $0 <build number> <reports path> <root>"
     exit 1
 }
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
     usage
 fi
 
 BUILD_NUMBER="$1"
-if [ -z "$BUILD_NUMBER" ]; then
+if [ -z "${BUILD_NUMBER}" ]; then
     usage
 fi
 
 REPORTS="$2"
-if [ -z "REPORTS" ]; then
+if [ -z "${REPORTS}" ]; then
+    usage
+fi
+
+ROOT="$3"
+if [ -z "${ROOT}" ]; then
     usage
 fi
 
 STATUS=0
 
 echo "======== TEST Couchbase Lite Java, Enterprise Edition v`cat ../../version.txt`-${BUILD_NUMBER}"
+export LD_LIBRARY_PATH="${ROOT}/common/lite-core/support/linux/x86_64:${LD_LIBRARY_PATH}"
 ./gradlew ciTest --info --console=plain || STATUS=1
 
 echo "======== Publish reports"
