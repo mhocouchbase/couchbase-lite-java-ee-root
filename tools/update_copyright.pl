468d4fc4285951d9b78f4a5f5900493175a1806a
G. Blake Meike <blake.meike@couchbase.com>
2020-08-14 12:13:22 -0700

    Update copyright

---
+++ tools/update_copyright.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -w
+use strict;
+
+while (<>) {
+    chomp;
+    my $file = $_;
+    next unless (-e $file);
+    next unless open (UPDATE, '<', $file);
+    next unless open (TMP, '>', ".copyright");
+    fix_copyright();
+    close UPDATE;
+    close TMP;
+    system "mv .copyright $file";
+}
+
+sub fix_copyright {
+    my $lines = 0;
+    while (<UPDATE>) {
+       my $copyright = /\/\/\s+Copyright\s+\(c\)\s+(\d{4})\s+Couchbase/;
+       if (($lines++ < 10) && (/\/\/\s+Copyright\s+\(c\)\s+(\d{4})/) && ($1 ne "2020")) {
+           s/(\/\/\s+Copyright\s+\(c\)\s+)(\d{4})/${1}2020, $2/;
+       }
+       print TMP $_;
+    }
+}
+
