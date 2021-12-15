8537f68e7a520015e303d4d16e512c21593a3e26
G. Blake Meike <blake.meike@couchbase.com>
2021-12-15 14:02:27 -0800

    More core update tool improvements

--- tools/update_core.sh
+++ tools/update_core.sh
@@ -3,66 +3,100 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT_DIR="${SCRIPT_DIR}/.."
 
-restore() {
-    pushd "${ROOT_DIR}/core" > /dev/null
-    git checkout master
-    popd > /dev/null
-    pushd  "${ROOT_DIR}/couchbase-lite-core-EE" > /dev/null
-    git checkout master
-    popd > /dev/null
+function usage() {
+    echo "Usage: $0 <core commit> <ee commit>"
+    exit 1
 }
 
+function fail() {
+    popd > /dev/null
+    exit 67
+}
 
-echo "=== Verify new core"
-pushd "${ROOT_DIR}/core" > /dev/null
-CUR_BRANCH="$( git symbolic-ref HEAD )"
-if [ "${CUR_BRANCH}" != "refs/heads/master" ]; then
-    echo "not on master branch in `pwd`"
-    restore
-    exit 1
+if [ "$#" -lt 1 -o "$#" -gt 2 ]; then
+    usage
 fi
-git checkout origin/master
-popd > /dev/null
 
-pushd  "${ROOT_DIR}/couchbase-lite-core-EE" > /dev/null
-CUR_BRANCH="$( git symbolic-ref HEAD )"
-if [ "${CUR_BRANCH}" != "refs/heads/master" ]; then
-    echo "not on master branch in `pwd`"
-    restore
-    exit 1
+CORE_COMMIT="$1"
+if [ -z "$CORE_COMMIT" ]; then
+    usage
 fi
-git checkout origin/master
-popd > /dev/null
 
-${SCRIPT_DIR}/verify_core.sh
-if [ $? -eq 0 ]; then
-    echo "incomplete core build"
-    restore
-    exit 1
+CORE_EE_COMMIT="$2"
+
+pushd "${ROOT_DIR}" > /dev/null
+
+cd core
+if [[ $(git status -s --ignore-submodules=dirty) != '' ]]; then
+    echo "Error: Core is dirty"
+    fail
 fi
+
+if [[ ! -z "$CORE_EE_COMMIT" ]]; then
+    if [[ $(git status -s --ignore-submodules=dirty) != '' ]]; then
+        echo "Error: Core-EE is dirty"
+        fail
+    fi
+fi
+
+cd ..
 
 echo "=== Update core"
-pushd "${ROOT_DIR}/core" > /dev/null
-git checkout master
-git reset --hard origin/master
-git submodule update --recursive
+cd core
+head=`git rev-parse HEAD`
+git checkout prev 2> /dev/null || git checkout -b prev
+git reset --hard $head
 git remote prune origin
-popd > /dev/null
+git fetch
+git checkout "$CORE_COMMIT" || fail
 
-echo "=== Update EE"
-pushd  "${ROOT_DIR}/couchbase-lite-core-EE" > /dev/null
-git checkout master
-git reset --hard origin/master
-git submodule update --recursive
-git remote prune origin
-popd > /dev/null
+if [[ ! -z "$CORE_EE_COMMIT" ]]; then
+    echo "=== Update core-EE"
+    cd  ../couchbase-lite-core-EE
+    head=`git rev-parse HEAD`
+    git checkout prev 2> /dev/null || git checkout -b prev
+    git reset --hard $head
+    git remote prune origin
+    git fetch
+    git checkout "$CORE_EE_COMMIT" || fail
+fi
+
+cd ..
+${SCRIPT_DIR}/verify_core.sh
+if [[ ! $? -eq 0 ]]; then
+    cd core
+    git checkout -b prev
+
+    if [[ ! -z "$CORE_EE_COMMIT" ]]; then
+        cd  ../couchbase-lite-core-EE
+        git checkout -b prev
+    fi
+
+    echo "Error: incomplete core build"
+    fail
+fi
 
 echo "=== Fetch new core"
-pushd "${ROOT_DIR}" > /dev/null
+cd core
+git checkout working 2> /dev/null || git checkout -b working
+git reset --hard "$CORE_COMMIT"
+git submodule update --recursive
+
+if [[ ! -z "$CORE_EE_COMMIT" ]]; then
+    cd  ../couchbase-lite-core-EE
+    git checkout working 2> /dev/null || git checkout -b working
+    echo "git reset --hard $CORE_EE_COMMIT"
+    git reset --hard "$CORE_EE_COMMIT"
+    git submodule update --recursive
+fi
+
+echo "=== Fetch"
+cd ..
 rm -rf common/lite-core
 # we used to build it...
 # common/tools/build_litecore.sh -e EE
 common/tools/fetch_litecore.sh -e EE -n "http://nexus.build.couchbase.com:8081/nexus/content/repositories/releases/com/couchbase/litecore"
 common/tools/build_litecore.sh -l mbedcrypto -e EE
+
 popd > /dev/null
 
