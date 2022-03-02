6a5913d600468093364ddae995fe82f900cf5673
LaurenNguyen14 <lauren.nguyen@couchbase.com>
2022-03-02 13:28:49 -0800

    Disable long-lived gradle daemon which hangs the script

--- ee/java/etc/jenkins/build_windows.ps1
+++ ee/java/etc/jenkins/build_windows.ps1
@@ -20,7 +20,7 @@ Write-Host "======== Download Lite Core"
 & $toolsDir/fetch_litecore.ps1 $LiteCoreRepoUrl -Edition "EE"
 
 Write-Host "======== Build Java"
-$process = Start-Process -FilePath "$PSScriptRoot\..\..\gradlew.bat" -ArgumentList "ciBuild --info -PbuildNumber=$buildNumber 2>&1 > .\build.log" -PassThru -Wait
+$process = Start-Process -FilePath "$PSScriptRoot\..\..\gradlew.bat" -ArgumentList "--no-daemon ciBuild --info -PbuildNumber=$buildNumber 2>&1 > .\build.log" -PassThru -Wait
 if($process.ExitCode -ne 0){
     Write-Host "Failed with error" $process.ExitCode
     exit $process.ExitCode
